e615a90d6460bd0e9986469eacb3419bedd1804d
Ali Soleimani <alisol911@gmail.com>
2023-12-07 13:49:03 +0330

    fixed fetch problem

--- components/Test.vue
+++ components/Test.vue
@@ -5,7 +5,7 @@ const props = defineProps({
 })
 
 const todoId = ref(props.id == "1" ? 1 : 2)
-const { data, pending, error } = await useFetch(() => `https://jsonplaceholder.typicode.com/todos/${todoId.value}`)
+const { data, pending, error } = await useFetch(() => `https://jsonplaceholder.typicode.com/todos/${todoId.value}`, { key: props.id } )
 
 onBeforeUpdate(() => {
   console.log(`onBeforeUpdate id: ${props.id} todoId: ${todoId.value}`)
